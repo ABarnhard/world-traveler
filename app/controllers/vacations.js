dae0ade10928b45bd92e098330f6e4387773df82
Adam Barnhard <admbarn@gmail.com>
2014-08-18 15:12:20 -0500

    added submit

--- app/controllers/vacations.js
+++ app/controllers/vacations.js
@@ -5,3 +5,7 @@ exports.init = function(req, res){
 };
 
 
+exports.create = function(req, res){
+  console.log(req.body);
+};
+
--- app/static/js/user/new-vacation.js
+++ app/static/js/user/new-vacation.js
@@ -9,10 +9,15 @@
   });
 
   function addVacation(e){
-    var name = $('input[name=name]').val();
-    geocode(name);
-    e.preventDefault();
+    var lat = $('input[name=lat]').val();
+
+    if(!lat){
+      var name = $('input[name=name]').val();
+      geocode(name);
+      e.preventDefault();
+    }
   }
+
   function geocode(address){
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({address: address}, function(results, status){
@@ -21,6 +26,15 @@
           lat = results[0].geometry.location.lat(),
           lng = results[0].geometry.location.lng();
       console.log(lat, lng, name);
+      $('input[name=name]').val(name);
+      $('input[name=lat]').val(lat);
+      $('input[name=lng]').val(lng);
+
+      // var data = $('form').serialize();
+      // console.log(data);
+
+      $('form').submit();
+
     });
   }
 })();
