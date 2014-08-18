5bbc4b13a88fb12df0beca879b87d393fbe06cf1
Adam Barnhard <admbarn@gmail.com>
2014-08-18 14:45:30 -0500

    fixed jscsrc

---
+++ app/static/js/user/new-vacation.js
@@ -0,0 +1,26 @@
+/* jshint unused:false, camelcase:false */
+/* global AmCharts:true, google:true */
+
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    $('form').submit(addVacation);
+  });
+
+  function addVacation(e){
+    var name = $('input[name=name]').val();
+    geocode(name);
+    e.preventDefault();
+  }
+  function geocode(address){
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address: address}, function(results, status){
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      var name = results[0].formatted_address,
+          lat = results[0].geometry.location.lat(),
+          lng = results[0].geometry.location.lng();
+      console.log(lat, lng, name);
+    });
+  }
+})();
